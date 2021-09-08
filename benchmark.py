490b0b0a0cd51bd3b1512ebfe6b75046cfd40a6d
ZongSingHuang <a26833765@gmail.com>
2021-09-08 22:02:31 +0800

    1

--- benchmark.py
+++ benchmark.py
@@ -175,21 +175,37 @@ def Mishra_N9(X):
 
 def SchmidtVetter(X):
     # [1]
-    # X in [-10, 10], D fixed 3
-    # X* = [1, 2, 3]
-    # F* = 0
+    # X in [0, 10], D fixed 3
+    # X* = [0.78547, 0.78547, 0.78547]
+    # F* = 3
     if X.ndim==1:
         X = X.reshape(1, -1)
         
     X1 = X[:, 0]
     X2 = X[:, 1]
     X3 = X[:, 2]
-    f1 = 2*X1**3 + 5*X1*X2 + 4*X3 - 2*X1**2*X3 - 18
-    f2 = X1 + X2**3 + X1*X2**2 + X1*X3**2 - 22
-    f3 = 8*X1**2 + 2*X2*X3 + 2*X2**2 + 3*X2**3 - 52
+    f1 = 1 + (X1-X2)**2
+    f2 = np.sin( (np.pi*X2+X3)/2 )
+    f3 = np.exp( ((X1+X2)/X2 - 2)**2 )
     
-    F = ( f1*f2**2*f3 + f1*f2*f3**2 + f2**2 + (X1+X2-X3)**2 )**2
+    F = 1/f1 + f2 + f3
             
+    return F
+
+def Wolfe(X):
+    # [1]
+    # X in [0, 2], D fixed 3
+    # X* = [0, 0, 0]
+    # F* = 0
+    if X.ndim==1:
+        X = X.reshape(1, -1)
+
+    X1 = X[:, 0]
+    X2 = X[:, 1]
+    X3 = X[:, 2]
+    
+    F = 4/3 * (X1**2 + X2**2 - X1*X2)**0.75 + X3
+    
     return F
 
 
@@ -1405,20 +1421,7 @@ def Whitley(X):
     
     return F
 
-def Wolfe(X):
-    # X in [0, 2], D fixed 3
-    # X* = [0, 0, 0]
-    # F* = 0
-    if X.ndim==1:
-        X = X.reshape(1, -1)
 
-    X1 = X[:, 0]
-    X2 = X[:, 1]
-    X3 = X[:, 2]
-    
-    F = 4/3 * (X1**2 + X2**2 - X1*X2)**0.75 + X3
-    
-    return F
 
 def Xin_She_Yang1(X):
     # X in [-5, 5]
--- 未命名0.py
+++ 未命名0.py
@@ -7,24 +7,24 @@ Created on Tue Sep  7 11:08:21 2021
 
 import numpy as np
 
-def Mishra_N9(X):
+def SchmidtVetter(X):
     # [1]
-    # X in [-10, 10], D fixed 3
-    # X* = [1, 2, 3]
-    # F* = 0
+    # X in [0, 10], D fixed 3
+    # X* = [0.78547, 0.78547, 0.78547]
+    # F* = 3
     if X.ndim==1:
         X = X.reshape(1, -1)
         
     X1 = X[:, 0]
     X2 = X[:, 1]
     X3 = X[:, 2]
-    f1 = 2*X1**3 + 5*X1*X2 + 4*X3 - 2*X1**2*X3 - 18
-    f2 = X1 + X2**3 + X1*X2**2 + X1*X3**2 - 22
-    f3 = 8*X1**2 + 2*X2*X3 + 2*X2**2 + 3*X2**3 - 52
+    f1 = 1 + (X1-X2)**2
+    f2 = np.sin( (np.pi*X2+X3)/2 )
+    f3 = np.exp( ((X1+X2)/X2 - 2)**2 )
     
-    F = ( f1*f2**2*f3 + f1*f2*f3**2 + f2**2 + (X1+X2-X3)**2 )**2
+    F = 1/f1 + f2 + f3
             
     return F
 
-X = np.zeros([5, 3]) + [1, 2, 3]
-F = Mishra_N9(X)
+X = np.zeros([5, 3]) + [0.78547, 0.78547, 0.78547]
+F = SchmidtVetter(X)
